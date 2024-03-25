cc5f1a083dae447455da132cb53d7991a5e37724
nzejoe <jonathannzete@gmail.com>
2024-03-25 05:34:57 -0700

    task 0 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,65 +1,30 @@
 #!/usr/bin/python3
+'''
+gather employee data from API
+'''
 
-"""
-Script to retrieve and display TODO list progress for a given employee
-using a REST API.
-
-Requirements:
-- Use urllib or requests module
-- Accept an integer as a parameter (employee ID)
-- Display progress information in the specified format
-"""
-
+import re
 import requests
 import sys
 
+REST_API = "https://jsonplaceholder.typicode.com"
 
-def get_employee_todo_progress(employee_id):
-    """ Fetches and displays TODO list progress for a given employee.
-
-    Args:
-    - employee_id (int): ID of the employee to retrieve TODO list for.
-
-    Returns:
-    - None
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
-
-    # Fetch user information
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    # Fetch TODO list for the employee
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
-
-    # Calculate progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(task.get("completed", False) for task in todos_data)
-
-    # Display progress information
-    print("Employee {} is done with tasks ({}/{}):".format(
-        employee_name, completed_tasks, total_tasks), end='\n')
-    # print("{}: {} completed tasks out of {}".format(
-    # employee_name, completed_tasks, total_tasks))
-
-    # Display titles of completed tasks
-    for task in todos_data:
-        if task.get('completed', False):
-            print("\t {}".format(task.get("title")))
-
-
-if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    # Extract employee ID from command-line arguments
-    employee_id = int(sys.argv[1])
-
-    # Call the function to get and display TODO list progress
-    get_employee_todo_progress(employee_id)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
